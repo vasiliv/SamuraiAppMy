8de156d827ed5a5f62890cd6eda0ce2736aae208
vasiliv <32122468+vasiliv@users.noreply.github.com>
2022-12-15 16:39:37 +0400

    Add project files.

---
+++ SamuraiApp.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SamuraiApp.UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
