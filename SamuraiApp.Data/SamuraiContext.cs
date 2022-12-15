d21db3911089797ad6f03c2fad0709575f6d8193
vasiliv <32122468+vasiliv@users.noreply.github.com>
2022-12-15 16:52:14 +0400

    2-7 finished

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public SamuraiContext(DbContextOptions<SamuraiContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotee { get; set; }
+    }
+}
