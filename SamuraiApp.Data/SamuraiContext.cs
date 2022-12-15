d05de337791f40128e10c4738cd162dfd85d48aa
vasiliv <32122468+vasiliv@users.noreply.github.com>
2022-12-15 17:06:01 +0400

    2-8 finished

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -10,11 +10,17 @@ namespace SamuraiApp.Data
 {
     public class SamuraiContext : DbContext
     {
+        private string connectionString = "server=(localdb)\\MSSQLLocalDB;database=SamuraiAppDataMy;Trusted_Connection=true";
         public SamuraiContext(DbContextOptions<SamuraiContext> options) : base(options)
         {
 
         }
         public DbSet<Samurai> Samurais { get; set; }
-        public DbSet<Quote> Quotee { get; set; }
+        public DbSet<Quote> Quote { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
     }
 }
