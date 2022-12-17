ca4d6fa0b55480eb4d99f4d5292fdc0d8b1f36e3
vasiliv <32122468+vasiliv@users.noreply.github.com>
2022-12-17 19:55:49 +0400

    4-5 finished

--- SamuraiApp.Data/Migrations/20221217071334_BattleSamuraiAdded.Designer.cs
+++ SamuraiApp.Data/Migrations/20221217071334_BattleSamuraiAdded.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SamuraiApp.Data;
 
 namespace SamuraiApp.Data.Migrations
 {
     [DbContext(typeof(SamuraiContext))]
-    partial class SamuraiContextModelSnapshot : ModelSnapshot
+    [Migration("20221217071334_BattleSamuraiAdded")]
+    partial class BattleSamuraiAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -17,10 +17,22 @@ namespace SamuraiApp.Data
         }
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quote { get; set; }
+        //public DbSet<BattleSamurai> BattleSamurais { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(connectionString);
             base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Samurai>()
+                .HasMany(s => s.Battles)
+                .WithMany(b => b.Samurais)
+                .UsingEntity<BattleSamurai>
+                    (bs => bs.HasOne<Battle>().WithMany(),
+                     bs => bs.HasOne<Samurai>().WithMany())
+                .Property(bs => bs.DateJoined)
+                .HasDefaultValueSql("getdate()");
         }
     }
 }
