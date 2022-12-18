b1dbead2e0874c6468298cefdd02b0033f77ff44
vasiliv <32122468+vasiliv@users.noreply.github.com>
2022-12-18 15:56:15 +0400

    4-7 finished

--- SamuraiApp.Domain/Horse.cs
+++ SamuraiApp.Domain/Horse.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,13 +6,11 @@ using System.Threading.Tasks;
 
 namespace SamuraiApp.Domain
 {
-    public class Samurai
+    public class Horse
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public List<Quote> Quotes { get; set; } = new List<Quote>();
-        public List<Battle> Battles { get; set; } = new List<Battle>();
         //one-to-one relationship
-        public Horse Horse { get; set;}
+        public int SamuraiId { get; set; }
     }
 }
